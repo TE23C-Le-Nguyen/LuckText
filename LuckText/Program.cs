fce3bacf38776722c9a50aa019bf7b54285569d0
Le gia Nguyen <le.gia@elev.ga.ntig.se>
2024-08-30 13:58:00 +0200

    Första

---
+++ LuckText/Program.cs
@@ -0,0 +1,29 @@
+﻿string name;
+
+Console.WriteLine("Skriv in ditt namn");
+
+name = Console.ReadLine();
+
+Console.Clear();
+
+Console.WriteLine($"Hello {name}!");
+
+Console.WriteLine("Vad är din nickname");
+
+string Nickname = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"W in the chat for {Nickname}!");
+
+Console.WriteLine("How much aura do you have?");
+
+string noob = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"{noob} aura is crazy lil bro");
+
+Console.WriteLine("What do you do in your free time?");
+
+string banana = Console.ReadLine();
+Console.Clear();
+Console.WriteLine($"{banana}ming in 2024 is crazy. Anyway {name} is a {Nickname} who got {noob} and 1 million power in Rise Of Kingdom. Bro like to {banana} in his free time tho, not very {Nickname}. ");
+
+Console.ReadLine();
